b304d7ffa6d50d2abb64e922ee82b22afa97ddb0
Raymond Juliano <juliano.raimundu@gmail.com>
2024-08-17 19:14:52 -0300

    criado o arquivo de deploy do kubernetes e o cluster ip

--- deploy-kubernetes.sh
+++ deploy-kubernetes.sh
@@ -3,7 +3,7 @@
 
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
-sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 sudo systemctl start docker
 curl -LO https://dl.k8s.io/release/v1.24.2/bin/linux/amd64/kubectl
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
@@ -12,4 +12,6 @@ curl -Lo minikube https://storage.googleapis.com/minikube/releases/v1.26.0/minik
 chmod +x minikube
 sudo install minikube /usr/local/bin/
 sudo usermod -aG docker $USER
+sudo systemctl enable --now docker
 sudo reboot
+minkube start
